db2b94e3b6136d407ce7da76dcf8313b6aa0a3ad
Francisco Vidal <programacionfv2022@gmail.com>
2023-03-31 13:43:14 -0300

    entregaunofranvidal

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import carrito from "./assets/cart2.png"
+
+export const CartWidget = () => {
+  return (
+    <div>
+    <img src={carrito} alt="carrito-widget"/>
+    <><b>2</b></>
+    </div>
+  )
+}
+
